4a8a2f8a78b719cdc3ba09a92e79ec618e6ebbc7
Pedro Carvalho <pedrofrancocarvalho@gmail.com>
2019-03-16 18:19:09 -0300

    Initial Commit

---
+++ blog.py
@@ -0,0 +1,72 @@
+import os
+import webapp2
+import jinja2
+from google.appengine.ext import db
+import datetime
+
+template_dir = os.path.join(os.path.dirname(__file__), "templates")
+jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
+                               autoescape=True)
+
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **params):
+        self.response.out.write(*a, **params)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **params):
+        self.write(self.render_str(template, **params))
+
+
+class Post(db.Model):
+    subject = db.StringProperty(required=True)
+    content = db.TextProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
+    last_modified = db.DateTimeProperty(auto_now=True)
+
+
+class MainPage(Handler):
+    def get(self):
+        posts = db.GqlQuery("SELECT * FROM Post ORDER BY created DESC LIMIT 10")
+        self.render("mainpage.html", posts=posts)
+
+
+class PostPage(Handler):
+    def get(self, post_id):
+        key = db.Key.from_path("Post", int(post_id))
+        post = db.get(key)
+
+        if not post:
+            self.error(404)
+            return
+        else:
+            self.render("permalink.html", post=post)
+
+
+class NewPost(Handler):
+    def get(self):
+        self.render("newpost.html")
+
+    def post(self):
+        subject = self.request.get("subject")
+        content = self.request.get("content")
+
+        if subject and content:
+            post = Post(subject=subject, content=content)
+            post.put()
+            self.redirect("/blog/%s" % str(post.key().id()))
+
+        else:
+            error = "we need both a subject and some content!"
+            self.render("newpost.html", error=error, subject=subject, content=content)
+
+
+# END OF FILE #
+
+app = webapp2.WSGIApplication([("/", MainPage),
+                               ("/newpost", NewPost),
+                               ("/([0-9]+)", PostPage)],
+                              debug=True)
