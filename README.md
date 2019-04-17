638fe14d130f5ce9dee360130ec1ee72229e3e6c
Pedro Carvalho <pedrofrancocarvalho@gmail.com>
2019-04-17 18:18:17 -0300

    Added README.md and bug fix on PostPage Render

---
+++ README.md
@@ -0,0 +1,20 @@
+## Allan Poe Blog
+
+#### DESCRIPTION
+For this project, my task was to create a Edgar Allan Poe Poem Blog. This blog is a Python webserver program using the `google.appengine.ext` as database and `Jinja2` framework to render the templates. Besides, I used `webapp2` module to set up the pages of blog.
+
+## REQUIREMENTS
+- Python 2.7
+- webapp2
+- webob
+- Google Cloud SDK
+- Jinja2
+- google.appengine.ext
+
+## RUNNING THE PROGRAM
+
+1. Setting Up a Python Development Environment [link](https://cloud.google.com/python/setup?hl=en-us)
+
+2. After install all Python modules use in the same folder that **blog.py** file are: `python "C:\Users\YOUR_USER\AppData\Local\Google\Cloud SDK\google-cloud-sdk\bin\dev_appserver.py" .`
+
+3. Don't forget to switch `YOUR_USER` and dot in the final.
--- blog.py
+++ blog.py
@@ -1,51 +1,61 @@
+#!/usr/bin/env python2.7
+
 import os
 import webapp2
 import jinja2
 from google.appengine.ext import db
 import datetime
 
+# Getting the template directory
 template_dir = os.path.join(os.path.dirname(__file__), "templates")
+
+# Starting the Jinja2 Environment
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
                                autoescape=True)
 
-
+# Main Handler
 class Handler(webapp2.RequestHandler):
+
+    # shortcut for writing the outputs easily
     def write(self, *a, **params):
         self.response.out.write(*a, **params)
 
+    # transform html file in a string to render
     def render_str(self, template, **params):
         t = jinja_env.get_template(template)
         return t.render(params)
 
+    # render the page
     def render(self, template, **params):
         self.write(self.render_str(template, **params))
 
-
+# Class that creates the database table
 class Post(db.Model):
     subject = db.StringProperty(required=True)
     content = db.TextProperty(required=True)
     created = db.DateTimeProperty(auto_now_add=True)
     last_modified = db.DateTimeProperty(auto_now=True)
 
-
+# Main Page Handler
 class MainPage(Handler):
     def get(self):
         posts = db.GqlQuery("SELECT * FROM Post ORDER BY created DESC LIMIT 10")
         self.render("mainpage.html", posts=posts)
 
-
+# PostPage Handler
 class PostPage(Handler):
     def get(self, post_id):
         key = db.Key.from_path("Post", int(post_id))
         post = db.get(key)
 
+        # Checking if there is a post with this key
         if not post:
             self.error(404)
             return
         else:
             self.render("permalink.html", post=post)
 
-
+# NewPost Page Handler
 class NewPost(Handler):
     def get(self):
         self.render("newpost.html")
@@ -54,6 +64,7 @@ class NewPost(Handler):
         subject = self.request.get("subject")
         content = self.request.get("content")
 
+        # Checking if user has filled all fields
         if subject and content:
             post = Post(subject=subject, content=content)
             post.put()
@@ -64,9 +75,10 @@ class NewPost(Handler):
             self.render("newpost.html", error=error, subject=subject, content=content)
 
 
-# END OF FILE #
+# End of File
 
+# Webserver start
 app = webapp2.WSGIApplication([("/", MainPage),
                                ("/newpost", NewPost),
-                               ("/([0-9]+)", PostPage)],
+                               ("/blog/([0-9]+)", PostPage)],
                               debug=True)
